83e3d8471209edaa40c7c143e0f096f8e4377097
Evgeniy <82458628+evgeniy-strel@users.noreply.github.com>
2022-05-11 16:48:22 +0500

    getting categories and deputies from the server when submitting an application

--- Application/UI/src/components/SubmissionsApplications/ChoiceStep/ChoiceStep.jsx
+++ Application/UI/src/components/SubmissionsApplications/ChoiceStep/ChoiceStep.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ChoiceStep = ({ activeItem, setActiveItem, items, choiceText, numberStep }) => {
+const ChoiceStep = ({
+  activeItem,
+  setActiveItem,
+  items,
+  choiceText,
+  numberStep,
+}) => {
   const getItemClass = (item) => {
-    let itemClass = 'button button_option blue';
-    if (item === activeItem) itemClass += ' button_option_active';
+    let itemClass = "button button_option blue";
+    if (item === activeItem) itemClass += " button_option_active";
     return itemClass;
   };
 
   const getPreviousNextUrl = () => {
     const urls = {
-      1: '/first_step',
-      2: '/second_step',
-      3: '/third_step',
-      4: '/fourth_step',
-      5: '/gratitude',
+      1: "/first_step",
+      2: "/second_step",
+      3: "/third_step",
+      4: "/fourth_step",
+      5: "/gratitude",
     };
 
     return {
-      previousUrl: numberStep > 1 ? urls[numberStep - 1] : '',
-      nextUrl: numberStep < 5 ? urls[numberStep + 1] : '',
+      previousUrl: numberStep > 1 ? urls[numberStep - 1] : "",
+      nextUrl: numberStep < 5 ? urls[numberStep + 1] : "",
     };
   };
 
@@ -28,7 +34,9 @@ const ChoiceStep = ({ activeItem, setActiveItem, items, choiceText, numberStep }
       <div className="container">
         <div className="step_choose first_step">
           <div className="choose_category">
-            <p className="title">Совсем скоро ты сможешь задать вопрос, но сначала</p>
+            <p className="title">
+              Совсем скоро ты сможешь задать вопрос, но сначала
+            </p>
             <p className="choose_cat">Выбери {choiceText}:</p>
             <ul className="list">
               {items.map((item, index) => (
@@ -37,23 +45,30 @@ const ChoiceStep = ({ activeItem, setActiveItem, items, choiceText, numberStep }
                   className={getItemClass(item)}
                   onClick={() => {
                     setActiveItem(item);
-                  }}>
-                  {item.name}
+                  }}
+                >
+                  {numberStep !== 3
+                    ? item.name
+                    : `${item.surname} 
+                       ${item.firstname && item.firstname[0]}. 
+                       ${item.patronymic && item.patronymic[0]}.`}
                 </li>
               ))}
             </ul>
             <div className="buttons_next_back">
               {numberStep != 1 && (
                 <Link
-                  to={getPreviousNextUrl()['previousUrl']}
-                  className="button button_next_back yellow">
+                  to={getPreviousNextUrl()["previousUrl"]}
+                  className="button button_next_back yellow"
+                >
                   Назад
                 </Link>
               )}
               {numberStep != 5 && (
                 <Link
-                  to={getPreviousNextUrl()['nextUrl']}
-                  className="button button_next_back yellow">
+                  to={getPreviousNextUrl()["nextUrl"]}
+                  className="button button_next_back yellow"
+                >
                   Далее
                 </Link>
               )}
--- Application/UI/src/components/SubmissionsApplications/ThirdStep/ThirdStep.jsx
+++ Application/UI/src/components/SubmissionsApplications/ThirdStep/ThirdStep.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import ThirdStepInfo from './ThirdStepInfo';
-import ChoiceStep from './../ChoiceStep/ChoiceStep';
+import React from "react";
+import ThirdStepInfo from "./ThirdStepInfo";
+import ChoiceStep from "./../ChoiceStep/ChoiceStep";
+import axios from "axios";
 
 const ThirdStep = ({ activeItem, setActiveItem }) => {
+  const [deputies, setDeputies] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get("http://localhost:5000/user")
+      .then(({ data }) => setDeputies(data));
+  }, []);
+
   return (
     <ChoiceStep
       activeItem={activeItem}
       setActiveItem={setActiveItem}
-      ulList={ThirdStepInfo}
+      items={deputies}
       choiceText="депутата"
       numberStep={3}
     />
