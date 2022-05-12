bbd1a04d70532899949b6770016f8ffe91c3d7e4
Vse-Vishnii <Lesha37b@yandex.ru>
2022-05-12 15:20:46 +0500

    fix deputies get and application post

--- Application/API/RezhDumaASPCore-Backend/RezhDumaASPCore-Backend/Controllers/UserController.cs
+++ Application/API/RezhDumaASPCore-Backend/RezhDumaASPCore-Backend/Controllers/UserController.cs
@@ -18,17 +18,9 @@ namespace RezhDumaASPCore_Backend.Controllers
         {
         }
 
-        [HttpGet("deputies")]
-        public async Task<ActionResult<IEnumerable<User>>> GetByRole()
-        {
-            return await repository.GetByRole(Role.Deputy);
-        }
-
         [HttpGet("deputies/filters/")]
         public async Task<ActionResult<IEnumerable<User>>> Get(string category = null, string district = null)
         {
-            if (category == null && district == null)
-                return BadRequest();
             return await repository.GetDeputyByCategoryAndDistrict(category, district);
         }
     }
