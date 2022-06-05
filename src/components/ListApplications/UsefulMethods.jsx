d77e6eca1a12fe302720b368e1f97e72e12cf2a6
Evgeniy <evgeniy_strel@yandex.ru>
2022-06-05 15:53:05 +0500

    fix search

--- src/components/ListApplications/UsefulMethods.jsx
+++ src/components/ListApplications/UsefulMethods.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
 export const GetNameStatus = (status) => {
   if (!isNaN(status)) {
     switch (status) {
       case 0:
-        return "В очереди";
+        return 'В очереди';
       case 1:
-        return "Назначен депутат";
+        return 'Назначен депутат';
       case 2:
-        return "Дан ответ";
+        return 'Дан ответ';
     }
   }
 };
@@ -16,29 +16,29 @@ export const GetNameStatus = (status) => {
 export const GetValueStatus = (status) => {
   if (status) {
     switch (status.toLowerCase()) {
-      case "в очереди":
-      case "новые заявки":
+      case 'в очереди':
+      case 'новые заявки':
         return 0;
-      case "назначен депутат":
+      case 'назначен депутат':
         return 1;
-      case "дан ответ":
+      case 'дан ответ':
         return 2;
     }
   }
 };
 
 export const getStringDeputy = (deputy) => {
-  return `${deputy.surname} 
+  return `${deputy.surname && deputy.surname} 
   ${deputy.firstname && deputy.firstname[0]}. 
   ${deputy.patronymic && deputy.patronymic[0]}.`;
 };
 
 export const getShortApplicationId = (applicationId) => {
-  return applicationId ? applicationId.split("-")[0].toUpperCase() : "";
+  return applicationId ? applicationId.split('-')[0].toUpperCase() : '';
 };
 
 export const getFormatDate = (application) => {
-  const [year, month, day] = application.created.substr(0, 10).split("-");
+  const [year, month, day] = application.created.substr(0, 10).split('-');
   return `${day}.${month}.${year}`;
 };
 
@@ -46,7 +46,6 @@ export const sortByTextApplications = (applications, text) => {
   text = text.toLowerCase();
   return applications.filter(
     (application) =>
-      application.id.toLowerCase().includes(text) ||
-      application.name.toLowerCase().includes(text)
+      application.id.toLowerCase().includes(text) || application.name.toLowerCase().includes(text),
   );
 };
